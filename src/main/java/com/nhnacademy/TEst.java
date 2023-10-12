170450f047411381418855a437dd7f0d97bf5fc8
{Danmoozzi} <{jaeyoon7421@naver.com}>
2023-10-12 13:20:47 +0900

    "."

--- src/main/java/com/nhnacademy/TEst.java
+++ src/main/java/com/nhnacademy/TEst.java
@@ -3,6 +3,6 @@ package com.nhnacademy;
 public class TEst {
 
     public static void main(String[] args) {
-        System.out.println("야 기분 좋다");
+        System.out.println("저희 다 파이팅 해보자구요!");
     }
 }
